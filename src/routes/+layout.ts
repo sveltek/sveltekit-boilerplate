e202c9ff5cd59fddedc5684839cf62173780db8e
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-06 12:46:14 +0200

    refactor: add 404 page

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,14 @@
+import { error } from '@sveltejs/kit'
+import type { LayoutLoad } from './$types'
+
 export const prerender = true
+
+export const load: LayoutLoad = async ({ url }) => {
+  if (!url.pathname) {
+    error(404, {
+      message: `Could not find ${url.pathname}.`,
+    })
+  }
+
+  return {}
+}
