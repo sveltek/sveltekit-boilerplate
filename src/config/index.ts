634f849f8bacea8007c92ea60c851311ea2ade75
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-06 12:23:15 +0200

    refactor: add app global config

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import type { ConfigApp } from './types'
+
+export const app: ConfigApp = {
+  name: 'SvelteKit Boilerplate',
+  author: 'Ivo Dolenc',
+  description: 'This is the latest SvelteKit starter template.',
+  url: 'https://sveltekit-boilerplate.dev',
+}
---
+++ src/config/types.ts
@@ -0,0 +1,6 @@
+export interface ConfigApp {
+  name: string
+  author: string
+  description: string
+  url: string
+}
