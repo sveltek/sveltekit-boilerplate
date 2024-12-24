c7edb643694832f16c4daf7a415b385ec87cdcb8
Ivo Dolenc <ivodolenc@gmail.com>
2024-12-24 21:07:56 +0100

    refactor: add comments to app config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,9 @@
 import type { ConfigApp } from './types'
 
+// NOTE!
+// Use this only for `public` data that can be safely exposed to the client-side.
+// Don't use `secrets` here!
+
 export const app: ConfigApp = {
   name: 'SvelteKit Boilerplate',
   author: 'Ivo Dolenc',
